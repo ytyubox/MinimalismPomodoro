82b5913a0dc1cf387fe5cd1c8f93a45a196a553b
Tsungyu Yu <ytyubox@gmail.com>
2019-02-23 18:02:10 +0800

    Initial Commit

---
+++ Minimalism Pomodoro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minimalism Pomodoro
+//
+//  Created by 游宗諭 on 2019/2/23.
+//  Copyright © 2019 游宗諭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
